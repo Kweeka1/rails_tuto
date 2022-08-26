163f1319371667d506f82c20da87e45a2cbd9a41
Kweeka <mtalbi29@yahoo.com>
2022-08-26 22:38:52 +0100

    added move validation for Queen piece and logic to check whether pieces are blocked during movement

--- app/javascript/chess.js
+++ app/javascript/chess.js
@@ -1,11 +1,10 @@
 const board = document.querySelector("#chess_container")
-let turn = "Red"
+const territories = document.querySelectorAll(".cube")
+let turn = "Blue"
 const RedTeam = "Red"
 const BlueTeam = "Blue"
 
-function validateKnightMove(lastPieceSelected, currentPieceSelected) {
-    const startingPosition = lastPieceSelected.attributes["coordination"].nodeValue.split(":")
-    const endPosition = currentPieceSelected.attributes["coordination"].nodeValue.split(":")
+function validateKnightMove(startingPosition, endPosition) {
 
     let rowMove = Math.abs(parseInt(startingPosition[0]) - parseInt(endPosition[0]))
     let colMove = Math.abs(parseInt(startingPosition[1]) - parseInt(endPosition[1]))
@@ -13,41 +12,102 @@ function validateKnightMove(lastPieceSelected, currentPieceSelected) {
     return rowMove + colMove === 3
 }
 
-function validateBishopMove(lastPieceSelected, currentPieceSelected) {
-    const startingPosition = lastPieceSelected.attributes["coordination"].nodeValue.split(":")
-    const endPosition = currentPieceSelected.attributes["coordination"].nodeValue.split(":")
+function validateBishopMove(startingPosition, endPosition, territories) {
 
     const rowMove = Math.abs(parseInt(startingPosition[0]) - parseInt(endPosition[0]))
     const colMove = Math.abs(parseInt(startingPosition[1]) - parseInt(endPosition[1]))
 
-    return rowMove === colMove
+    const startingRowPos = parseInt(startingPosition[0])
+    const startingColPos = parseInt(startingPosition[1])
+
+    const endingRowPos = parseInt(endPosition[0])
+    const endingColPos = parseInt(endPosition[1])
+
+    const isBlocked = function () {
+        let col = startingColPos
+
+        if (startingRowPos > endingRowPos) {
+            for (let i = startingRowPos - 1; i > endingRowPos; i--) {
+                if (endingColPos > startingColPos) {
+                    col++
+                    if (territories[(i * 8) + col].attributes["team"].nodeValue !== "") return true
+                } else {
+                    col--
+                    if (territories[(i * 8) + col].attributes["team"].nodeValue !== "") return true
+                }
+            }
+        } else {
+            for (let i = startingRowPos + 1; i < endingRowPos; i++) {
+                if (endingColPos > startingColPos) {
+                    col++
+                    if (territories[(i * 8) + col].attributes["team"].nodeValue !== "") return true
+                } else {
+                    col--
+                    if (territories[(i * 8) + col].attributes["team"].nodeValue !== "") return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    return rowMove === colMove && !isBlocked()
 }
 
-function validateRockMove(lastPieceSelected, currentPieceSelected) {
-    const startingPosition = lastPieceSelected.attributes["coordination"].nodeValue.split(":")
-    const endPosition = currentPieceSelected.attributes["coordination"].nodeValue.split(":")
+function validateRockMove(startingPosition, endPosition) {
 
     const startingRowPos = parseInt(startingPosition[0])
     const startingColPos = parseInt(startingPosition[1])
+
     const endingRowPos = parseInt(endPosition[0])
     const endingColPos = parseInt(endPosition[1])
 
     const rowMove = startingRowPos !== endingRowPos && startingColPos === endingColPos
     const colMove = startingRowPos === endingRowPos && startingColPos !== endingColPos
 
-    return rowMove || colMove
+    function isBlocked() {
+        if (endingRowPos > startingRowPos) {
+            for (let i = startingRowPos + 1; i < endingRowPos; i++) {
+                if (territories[(i * 8) + startingColPos].attributes["team"].nodeValue !== "") return true
+            }
+        } else {
+            for (let i = startingRowPos - 1; i > endingRowPos; i--) {
+                if (territories[(i * 8) + startingColPos].attributes["team"].nodeValue !== "") return true
+            }
+        }
+        if (endingColPos > startingColPos) {
+            for (let i = startingColPos + 1; i < endingColPos; i++) {
+                console.log(territories[(i * 8) + startingRowPos])
+                if (territories[(i * 8) + startingRowPos].attributes["team"].nodeValue !== "") return true
+            }
+        } else {
+            for (let i = startingColPos - 1; i > endingColPos; i--) {
+                console.log(territories[(startingRowPos * 8) + i])
+                if (territories[(startingRowPos * 8) + i].attributes["team"].nodeValue !== "") return true
+            }
+        }
+
+        return false
+    }
+
+    return rowMove || colMove ? !isBlocked() : false
 }
 
-function validateKingMove(lastPieceSelected, currentPieceSelected) {
-    const isRockMove = validateRockMove(lastPieceSelected, currentPieceSelected)
-    const isBishopMove = validateBishopMove(lastPieceSelected, currentPieceSelected)
+function validateKingMove(startingPosition, endPosition) {
+    if (startingPosition[0] === endPosition[0] || startingPosition[1] === endPosition[1])
+        return validateRockMove(startingPosition, endPosition)
 
-    return isRockMove || isBishopMove
+    else return validateBishopMove(startingPosition, endPosition, territories)
 }
 
-function validatePawn(lastPieceSelected, currentPieceSelected) {
-    const startingPosition = lastPieceSelected.attributes["coordination"].nodeValue.split(":")
-    const endPosition = currentPieceSelected.attributes["coordination"].nodeValue.split(":")
+function validateQueenMove(startingPosition, endPosition) {
+    const rowMove = Math.abs(parseInt(startingPosition[0]) - parseInt(endPosition[0]))
+    const colMove = Math.abs(parseInt(startingPosition[1]) - parseInt(endPosition[1]))
+
+    return (rowMove === 0 || rowMove === 1) && (colMove === 0 || colMove === 1)
+}
+
+function validatePawnMove(startingPosition, endPosition, isSelectedTerritoryTeam) {
 
     const startingRowPos = parseInt(startingPosition[0])
     const startingColPos = parseInt(startingPosition[1])
@@ -57,39 +117,45 @@ function validatePawn(lastPieceSelected, currentPieceSelected) {
     const rowMove = Math.abs(startingRowPos - endingRowPos)
     const colMove = Math.abs(startingColPos - endingColPos)
 
-    const isSelectedTerritoryTeam = currentPieceSelected.attributes["team"].nodeValue
-
     if (turn === "Blue") {
+
         if (endingRowPos < startingRowPos) return false
         if (colMove === 1 && rowMove === 1) return isSelectedTerritoryTeam === RedTeam
-        if (startingRowPos === 1)
+        if (startingRowPos === 1) {
             return colMove === 0 && isSelectedTerritoryTeam !== RedTeam && rowMove === 1 || rowMove === 2
-
+        }
         return colMove === 0 && isSelectedTerritoryTeam !== RedTeam && rowMove === 1
 
     } else {
 
         if (endingRowPos > startingRowPos) return false
         if (colMove === 1 && rowMove === 1) return isSelectedTerritoryTeam === BlueTeam
-        if (startingRowPos === 6 && colMove === 0)
-            return  isSelectedTerritoryTeam !== BlueTeam && rowMove === 1 || rowMove === 2
-
+        if (startingRowPos === 6 && colMove === 0) {
+            return isSelectedTerritoryTeam !== BlueTeam && rowMove === 1 || rowMove === 2
+        }
         return colMove === 0 && isSelectedTerritoryTeam !== BlueTeam && rowMove === 1
+
     }
 }
 
 function validateMove(lastPiece, lastPieceSelected, currentPieceSelected) {
+    const isSelectedTerritoryTeam = currentPieceSelected.attributes["team"].nodeValue
+    const startingPosition = lastPieceSelected.attributes["coordination"].nodeValue.split(":")
+    const endPosition = currentPieceSelected.attributes["coordination"].nodeValue.split(":")
+
     switch (lastPiece) {
         case "Knight":
-            return validateKnightMove(lastPieceSelected, currentPieceSelected)
+            return validateKnightMove(startingPosition, endPosition)
         case "Bishop":
-            return validateBishopMove(lastPieceSelected, currentPieceSelected)
+            return validateBishopMove(startingPosition, endPosition, territories)
         case "Rock":
-            return validateRockMove(lastPieceSelected, currentPieceSelected)
+            return validateRockMove(startingPosition, endPosition)
         case "King":
-            return validateKingMove(lastPieceSelected, currentPieceSelected)
+            return validateKingMove(startingPosition, endPosition)
         case "Pawn":
-            return validatePawn(lastPieceSelected, currentPieceSelected)
+            return validatePawnMove(startingPosition, endPosition, isSelectedTerritoryTeam)
+        case "Queen":
+            return validateQueenMove(startingPosition, endPosition)
     }
 }
 
@@ -101,9 +167,11 @@ function selectOtherPiece(currentPiece) {
 
 function movePieceToNewPos(currentPiece, lastPieceTeam, lastPiece) {
     lastPieceSelected.style.border = ""
+
     currentPiece.innerHTML = lastPieceSelected.innerHTML
     currentPiece.setAttribute("team", lastPieceTeam)
     currentPiece.setAttribute("piece", lastPiece)
+
     lastPieceSelected.innerHTML = ""
     lastPieceSelected.setAttribute("team", "")
     lastPieceSelected.setAttribute("piece", "")
